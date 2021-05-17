35362cc1b3febaad8182d6f6002a62a6c6850676
EmAdthasit <em.adthasit@gmail.com>
2021-05-18 01:05:22 +0700

    update init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,97 +2,78 @@ import React, { useState } from 'react';
 import './App.css';
 import "./tailwind.css"
 
-import Slider from './Slider'
-
 import { useVisibility } from './core/hooks/useVisibility';
-import { useCoreService, useDataService } from './core/hooks/useCoreService';
+import { useCoreService } from './core/hooks/useCoreService';
 
-import { useData, useJsonData } from './core/hooks/useData';
-import { useSetRecoilState, useRecoilValue } from "recoil";
-import { coreState, charCountState } from './core/hooks/state';
-
-const DataForm = () => {
-  const data = useData()
-  const [name, setName] = useState(data);
-  const setData = useSetRecoilState(coreState.data);
-  const setVisibility = useSetRecoilState(coreState.visibility);
-
-  const saveData = (e) => {
-    e.preventDefault();
-    setData(name);
-  };
-
-  const onHide = (e) => {
-    e.preventDefault();
-    setVisibility(false);
-  };
-
-  return( 
-    <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Username
-          </label>
-          <input value={name} onChange={(event) => setName(event.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-        </div>
-        <div className="flex items-center justify-between">
-          <button onClick={saveData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Save</button>
-          <button onClick={onHide} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Hide</button>
-        </div>
-      </form>
-    </div>
-  )
-}
-
-function JsonData() {
-  const jsonData = useJsonData()
-  const setJsonDataCount = useSetRecoilState(coreState.jsonData);
-
-  const onPlus = () => {
-    setJsonDataCount({
-      count: jsonData.count + 1
-    })
-  }
-  
-  const Minus = () => {
-    setJsonDataCount({
-      count: jsonData.count - 1
-    })
-  }
-  
-  return (
-    <div>
-      <p>You clicked {jsonData.count} times</p>
-      <button onClick={() => onPlus()} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-        Plus
-      </button>
-      <button onClick={() => Minus()} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-        Minus
-      </button>
-    </div>
-  );
-}
+import { useRecoilValue } from "recoil";
 
 function App() {
-  useCoreService();
-  useDataService();
+  useCoreService()
   const visibility = useVisibility()
-  const data = useData()
-  const jsonData = useJsonData()
-
-  const count = useRecoilValue(charCountState);
 
   return (
     <div style={ visibility ? { visibility: 'visible' } : { visibility: 'hidden' }}>
-     {/** Any types of components goes here. Maybe some routing? */}
-      <h1 style={{ color: 'black'}}>FiveM React Boilerplate</h1>
-      <DataForm/>
-      <h2>{data}</h2>
-      <h2>Character Count: {count}</h2>
-      <JsonData/> 
-      <h2>JsonData Count: {jsonData.count}</h2>
-      <Slider/>
+      <div className="container mx-auto">
+        <div className="md:container md:mx-auto">
+          <div className="box-content h-32 w-full p-4 border-2 rounded-3xl flex justify-between bg-gradient-to-r from-white to-black">
+            {/* Left */}
+            <div className="flex justify-around content-center items-center w-1/3">
+              {/* Logo Left */}
+              <div>
+                <img className="object-scale-down h-20" src="https://cdn.discordapp.com/attachments/775913267079544842/843907068200353842/DRAGON.png"/>
+              </div>
+              {/* Score Left */}
+              <div className="flex content-center items-center">
+                <h1 style={{ fontSize: 50, color: 'black', fontWeight: "bold"}}>0</h1>
+              </div>
+            </div>
+            {/* Center */}
+            <div className="flex flex-col justify-center justify-items-center items-center content-center w-1/2">
+              <h1 className="flex justify-center leading-6" style={{ fontSize: 19, fontWeight: 600, color: '#FFF'}}>Crown of King 2021</h1>
+              <h1 className="flex justify-center leading-6" style={{ fontSize: 24, fontWeight: "bold", color: '#FFF'}}>Qualifiers</h1>
+              <h1 className="flex justify-center leading-6" style={{ fontSize: 16, fontWeight: 600, color: '#FFF'}}>Familie City</h1>
+            </div>
+            {/* Right */}
+            <div className="flex flex-row-reverse justify-around content-center items-center w-1/3">
+              {/* Logo Right */}
+              <div>
+                <img className="object-scale-down h-20" src="https://cdn.discordapp.com/attachments/775913267079544842/843907074874408990/JAK.png"/>
+              </div>
+              {/* Score Right */}
+              <div>
+                <div className="flex content-center items-center">
+                  <h1 style={{ fontSize: 50, color: '#FFF', fontWeight: "bold"}}>0</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row box-content px-4 h-6 w-full rounded-3xl flex justify-between">
+            <div className="flex w-1/2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+            </div>
+            <div className="flex w-1/2 justify-end">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 fill-current text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
+              </svg>
+            </div>
+          </div>
+          {/* <img src="https://i.pinimg.com/564x/e6/1a/4e/e61a4e31569a4431b18ce6f8a8216f39.jpg"/> */}
+        </div>
+      </div>
     </div>
   );
 }
