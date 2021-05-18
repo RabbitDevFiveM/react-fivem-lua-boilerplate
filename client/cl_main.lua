a03bbab008340e8c2567fa19b2807cf345eb23fa
EmAdthasit <em.adthasit@gmail.com>
2021-05-18 19:42:44 +0700

    alt [focus]

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,6 +1,37 @@
 -- Example of how it works. Look at the `useCoreService`, and the nui function in `nui-events`
+local focus = false
+local isDead = false
+local showControl = false
+Citizen.CreateThread(function()
+  while(true) do
+      isDead = IsPedDeadOrDying(PlayerPedId())
+      if isDead and showControl then
+          close()
+      end
+      Citizen.Wait(500)
+  end
+end)
+
+Citizen.CreateThread(
+    function()
+        while true do
+            Citizen.Wait(1)
+            if IsControlJustReleased(1, 19) then
+              Citizen.Wait(300)
+              if focus then
+                SetNuiFocus( false, false )
+              else
+                SetNuiFocus( true, true )
+              end
+              focus = not focus
+            end
+        end
+    end
+)
 
 RegisterCommand('score:show', function(source, args, rawCommand)
+  focus = true
+  showControl = true
   SetNuiFocus( true, true )
   SendNUIMessage({
     app = "REACTNUI",
@@ -10,15 +41,8 @@ RegisterCommand('score:show', function(source, args, rawCommand)
 end, false)
 
 RegisterCommand('score:hide', function(source, args, rawCommand)
-  SendNUIMessage({
-    app = "REACTNUI",
-    method = "setVisibility",
-    data = false
-  })
-end, false)
-
-
-RegisterCommand('score:left', function(source, args, rawCommand)
+  focus = false
+  showControl = false
   SendNUIMessage({
     app = "REACTNUI",
     method = "setVisibility",
@@ -31,7 +55,18 @@ RegisterNUICallback("Close", function(data, cb)
   cb('OK')
 end)
 
+RegisterNUICallback("Focus", function(data, cb)
+  if focus then
+    SetNuiFocus( false, false )
+  else
+    SetNuiFocus( true, true )
+  end
+  focus = not focus
+end)
+
 function close ()
+  showControl = false
+  focus = false
   SetNuiFocus( false, false )
   SendNUIMessage({
     app = "REACTNUI",
