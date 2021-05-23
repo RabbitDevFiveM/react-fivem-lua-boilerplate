56eb7e1bef68a8cc6d26b6bc67840a2201439646
EmAdthasit <66411403+EmAdthasit@users.noreply.github.com>
2021-05-23 07:16:01 +0700

    press esc for close ui

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,6 +1,7 @@
 -- Example of how it works. Look at the `useCoreService`, and the nui function in `nui-events`
 
 RegisterCommand('show:nui', function(source, args, rawCommand)
+  SetNuiFocus( true, true )
   SendNUIMessage({
     app = "REACTNUI",
     method = "setVisibility",
@@ -9,9 +10,20 @@ RegisterCommand('show:nui', function(source, args, rawCommand)
 end, false)
 
 RegisterCommand('close:nui', function(source, args, rawCommand)
+  SetNuiFocus( false, false )
   SendNUIMessage({
     app = "REACTNUI",
     method = "setVisibility",
     data = false
   })
 end, false)
+
+RegisterNUICallback("Close", function(data, cb)
+  SetNuiFocus( false, false )
+  SendNUIMessage({
+    app = "REACTNUI",
+    method = "setVisibility",
+    data = false
+  })
+  cb('OK')
+end)
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
 
 // show and hide
 import { useVisibility } from './core/hooks/useVisibility';
 import { useCoreService } from './core/hooks/useCoreService';
 
+// NUI
+import { useNuiRequest } from "fivem-nui-react-lib";
+
 function App() {
   useCoreService();
   const visibility = useVisibility()
+  const { send } = useNuiRequest();
+
+  const escFunction = useCallback((event) => {
+    if (event.keyCode === 27) {
+      //Do whatever when esc is pressed
+      send("Close", {})
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, []);
+
+  
   return (
     <div style={ visibility ? { visibility: 'visible' } : { visibility: 'hidden' }}>
      {/** Any types of components goes here. Maybe some routing? */}
